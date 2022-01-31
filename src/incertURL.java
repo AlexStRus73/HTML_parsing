bfb64bcc34bd5aa99754ae773c4309e4f2a8dc14
Alexei Stolyarov <a.i.stolyarov73@gmail.com>
2022-01-31 22:00:51 +0400

    Full

--- src/incertURL.java
+++ src/incertURL.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class incertURL {
+public class incertURL {
+    private String URL;
+
+    public incertURL (String URL){              //Объявляем конструктор
+        if (validURL(URL)) this.URL = URL;
+        else this.URL = "";
+    }
+
+    public boolean validURL (String URL) {      // Метод, проверяющий URL на валидность
+        if(URL.matches("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]")) {
+            System.out.println("String contains URL");
+            return true;
+        } else {
+            System.out.println("Unvalid URL");
+            return false;
+        }
+
+    }
+
+
+    public String getURL () {
+        return URL;
+    }
+
 }
--- src/main.java
+++ src/main.java
@@ -1,2 +1,38 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.io.*;
+import java.util.*;
+
 public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Please, input web adress...");
+        Scanner scanner = new Scanner(System.in);
+        incertURL incert = new incertURL(scanner.nextLine());
+        scanner.close();
+        HashMap<String,Integer > Names = new HashMap<>();
+
+        try{
+            if (!incert.getURL().isEmpty()) {
+                Document doc = Jsoup.connect(incert.getURL()).get();
+                String text = doc.text();                            // получаем текст страницы
+                for (String retval : text.split("\\s*(\\s|,|\\.|!|\\?|\\\"|\\;|\\:|\\[|\\]|\\(|\\)|\n|\r|\t)\\s*")) {
+                    if (Names.containsKey(retval)) {                //если значение ключа ранее уже было внесено - прибавляй 1
+                        int Name = Names.get(retval);
+                        Names.put(retval,(Name+1));
+                    } else {Names.put(retval, 1);}                  //иначе вносим
+                }
+                for (Map.Entry entry: Names.entrySet()) {           //вывод HashMap в консоль
+                    System.out.println(entry);
+                }
+                }
+        } catch(IOException e){
+        System.out.println("Ошибка ввода/вывода: "+ e);
+    }
+    }
 }
+
+
+
+// ' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t'
+//https://simbirsoft.com
